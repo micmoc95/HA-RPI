6d047d8c597376d7e567b0237b3f7378338b6a05
micmoc95 <mic.morat@gmail.com>
2025-12-18 17:03:42 +0100

    Update run.sh

--- Haap/run.sh
+++ Haap/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-sleep infinity
-
 nmcli dev wifi haap ifname wlan0 ssid MICRPI password kenobite
 nmcli connection modify haap ipv4.addresses 192.168.169.170/24
 nmcli connection modify haap ipv4.method shared
 nmcli connection up haap
+
+sleep infinity
