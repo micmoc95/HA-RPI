97de23b1b9881f60aa98581683641baec39263fe
micmoc95 <mic.morat@gmail.com>
2026-01-12 07:42:34 +0100

    Update __init__.py

--- HaDialer/hadialer/__init__.py
+++ HaDialer/hadialer/__init__.py
@@ -7,6 +7,6 @@ async def async_setup(hass, config):
     @callback
     async def dial(call: ServiceCall):
         async with aiohttp.ClientSession() as s:
-            await s.post("http://127.0.0.1:8124/dial", json={'num': '0769432853'})
+            await s.post("http://127.0.0.1:8124/dial", json={"num": call.data ["num"]})
     hass.services.async_register("hadialer", "dial", dial)
     return True
